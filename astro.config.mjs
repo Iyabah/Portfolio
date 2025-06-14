1877b4aa0a3ed23caaa9bb660fee30fcacc0bb35
Iya Said Bah <iyabah91@gmail.com>
2025-06-14 18:30:54 +0000

    version update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
-
-
+import vercel from '@astrojs/vercel/serverless'; // Add this import
 
 export default defineConfig({
-  site: 'https://iyabahsaid.site', 
-  integrations: [sitemap({
-    changefreq: 'weekly',
-    priority: 0.7,
-    lastmod: new Date(),
-  })
-
+  site: 'https://iyabahsaid.site',
+  output: 'server', // Add this line - enables SSR
+  adapter: vercel(), // Add this line - Vercel adapter
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
   ],
 });
